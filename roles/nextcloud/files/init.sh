40cd623da30e5a359eba859c58da89f83a0e939b
hliblack <hliblack@outlook.com>
2019-10-30 17:13:42 +0800

    v1.4 fix bugs

--- roles/nextcloud/files/init.sh
+++ roles/nextcloud/files/init.sh
@@ -8,17 +8,14 @@ new_nextcloud_password=$(pwgen -Acns 8 1)
 systemctl restart mysql
 
 root_hosts=$(echo $(mysql -uroot -p${old_password} -e "select host from mysql.user where user='root';") | sed 's/^host//')
-nextcloud_hosts=$(echo $(mysql -unextcloud -p${old_password} -e "select host from mysql.user where user='nextcloud';") | sed 's/^host//')
 
 for i in $root_hosts
 do
   mysqladmin -uroot -p${old_password} -h $i password $new_root_password
 done
 
-for j in $nextcloud_hosts
-do
-  mysqladmin -unextcloud -p${old_password} -h $j password $new_nextcloud_password
-done
+mysqladmin -unextcloud -p${old_password} password $new_nextcloud_password
+
 
 echo -e 'MySQL username:root\nMySQL Password:'$new_root_password  > /credentials/password.txt
 echo -e '\nMySQL username:nextcloud\nMySQL Password:'$new_nextcloud_password  >> /credentials/password.txt
